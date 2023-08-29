8c03d24f70f060af3c7821111179a874f167eb20
Ritis Barauskas <perm300ru@yandex.ru>
2023-08-29 20:33:28 +0300

    MYLDON-2: refactoring

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 class User:
-    def __init__(self, username, password,age):
+    def __init__(self, username, password, age):
         self.username = username
         self.password = password
         self.first_name = ''
