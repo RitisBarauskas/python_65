808b2916f98072b437b0a3fb1d19fea988ce95fa
Ritis Barauskas <perm300ru@yandex.ru>
2023-08-29 20:25:11 +0300

    MYLDON-2: добавил поля имени и фамилии в модель пользователя

--- models.py
+++ models.py
@@ -2,6 +2,8 @@ class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        self.first_name = ''
+        self.last_name = ''
 
     def __str__(self):
         return f'User: {self.username}'
