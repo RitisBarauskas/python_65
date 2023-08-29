dca93604e0bf4cd3ae321e562eeb5ec22a976e41
Александр Русанов <shurik.82rusanov@yandex.ru>
2023-08-29 20:26:21 +0300

    add_age

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 class User:
-    def __init__(self, username, password):
+    def __init__(self, username, password,age):
         self.username = username
         self.password = password
+        self.age = age
+
 
     def __str__(self):
         return f'User: {self.username}'
